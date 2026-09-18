6b9ad4321f5ef42010f900da9611a4f348122bfc
Kushal B G <kushalbg06@gmail.com>
2026-09-18 20:19:19 +0530

    solved Missing Number

---
+++ q_no_268.cpp
@@ -0,0 +1,22 @@
+#268 Missing Number
+class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            int n = nums.size();
+            for (int i = 0; i <= n; i++) {
+                bool found = false;
+                for (int j = 0; j < n; j++) {
+                    if (nums[j] == i) {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    };
+#time complexity:O(n**2)
+#space complexity:O(1)
