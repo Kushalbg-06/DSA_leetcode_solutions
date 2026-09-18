2a1f19ad56c3448f0bf28e8e1106a805fed8c3ca
Kushal B G <kushalbg06@gmail.com>
2026-09-18 20:40:54 +0530

    solved Single Number

--- q_no_136.cpp
+++ q_no_136.cpp
@@ -1,10 +1,10 @@
 #136. Single Number
+#brute force approach
 class Solution {
     public:
         int singleNumber(vector<int>& nums) {
             int n = nums.size();
             int count = 0;
-            int val;
             for (int i = 0; i < n; i++) {
                 count = 0;
                 for (int j = 0; j < n; j++) {
@@ -19,3 +19,20 @@ class Solution {
             return -1;
         }
     };
+#time complexity:O(n**2)
+#space complexity:O(1)
+
+#optimal approach
+class Solution {
+    public:
+        int singleNumber(vector<int>& nums) {
+            int n = nums.size();
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                res ^= nums[i];
+            }
+            return res;
+        }
+    };
+#time complexity:O(n)
+#space complexity:O(1)
