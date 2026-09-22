353a89764e26a22203c6ac4a7aaae49fb6348970
Kushal B G <kushalbg06@gmail.com>
2026-09-22 20:35:08 +0530

    solved Min Cost Climbing Stairs

---
+++ q_no_746.cpp
@@ -0,0 +1,13 @@
+#746. Min Cost Climbing Stairs
+class Solution {
+    public:
+        int minCostClimbingStairs(vector<int>& cost) {
+            int n=cost.size();
+            for(int i=2;i<n;i++){
+                cost[i]+=min(cost[i-1],cost[i-2]);
+            }
+            return min(cost[n-1],cost[n-2]);
+        }
+    };
+#time complexity:O(n)
+#space complexity:O(1)
