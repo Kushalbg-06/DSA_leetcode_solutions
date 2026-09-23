1c8d1d1178c68e33095c5d7b8064b9c5204c1fee
Kushal B G <kushalbg06@gmail.com>
2026-09-23 22:30:25 +0530

    solved N-th Tribonacci Number

---
+++ q_no_1137.cpp
@@ -0,0 +1,24 @@
+#1137. N-th Tribonacci Number
+class Solution {
+    public:
+        int tribonacci(int n) {
+            if (n == 0) {
+                return 0;
+            } 
+            else if (n == 1 || n == 2) {
+                return 1;
+            }
+            int a=0,b=1,c=1;
+            int nextTrib;
+            
+            for (int i = 3; i <= n; i++) {
+                nextTrib = a + b + c;
+                a=b;
+                b=c;
+                c=nextTrib;
+            }        
+            return c;
+        }
+    };
+#time complexity:O(n)
+#space complexity:O(1)
